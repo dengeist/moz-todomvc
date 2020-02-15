f331f6208a477b34385bd15e16f966f00879fe4d
EJ Mason <eliasjmason@gmail.com>
2020-02-14 18:14:46 -0800

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Button from "./Button.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+  <form>
+  <label for="new-todo">What needs to be done?</label>
+  <input type="text" id="new-todo" name="text">
+  <button type="submit">Add</button>
+  </form>
+  <main>
+</main>
